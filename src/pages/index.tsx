66a26df7b25aee3474388ab96a67aa379cd82839
tultul666 <antonello.android@gmail.com>
2025-01-06 13:41:51 +0100

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RecetteOrecchiette from '../components/recipe-blog';
+
+export default function Home() {
+  return (
+    <main>
+      <RecetteOrecchiette />
+    </main>
+  );
+}
