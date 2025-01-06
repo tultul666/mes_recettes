e3d73be35da07c1bbd7af6a8d0e4d363bc967e1b
tultul666 <antonello.android@gmail.com>
2025-01-06 13:53:24 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import RecetteOrecchiette from '../components/recipe-blog';
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>
-      <RecetteOrecchiette />
-    </main>
+    <div className="min-h-screen bg-[#fff2f2]">
+      <header className="p-4 text-center">
+        <h1 className="text-3xl font-bold text-[#ff7b7b]">Mes Recettes Italiennes</h1>
+      </header>
+      <main>
+        <RecetteOrecchiette />
+      </main>
+    </div>
   );
-}
+};
+
+export default Home;
